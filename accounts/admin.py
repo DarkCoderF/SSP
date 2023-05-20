904e7324bc6b6f62cd6af891396b8d0cfdbfa4e7
DarkCoderF <132500216+DarkCoderF@users.noreply.github.com>
2023-05-20 18:14:30 +0330

    Delete accounts directory

--- accounts/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import UserInfo
-
-
-class UserInfoAdmin(admin.ModelAdmin):
-    list_display = ('user', 'about', 'photo', 'phone')
-    list_filter = ('phone',)
-    
-admin.site.register(UserInfo, UserInfoAdmin)
